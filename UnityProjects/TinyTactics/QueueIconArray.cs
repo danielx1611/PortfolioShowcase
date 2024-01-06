58ddc6c8243d5670b4973e5478377bcefbc87829
RocketBlaster05 <82424308+RocketBlaster05@users.noreply.github.com>
2024-01-06 16:02:49 -0500

    Update QueueIconArray.cs

--- UnityProjects/TinyTactics/QueueIconArray.cs
+++ UnityProjects/TinyTactics/QueueIconArray.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 
 public class QueueIconArray : MonoBehaviour
 {
+    // Public reference to the list of icons at the top of the screen indicating the troop turn queue
     public TroopMovementQueueButton[] array = new TroopMovementQueueButton[12];
 }
