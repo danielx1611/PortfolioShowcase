b56cdd783ad99b1c9f459a90ad6b8f0c15ba3179
RocketBlaster05 <82424308+RocketBlaster05@users.noreply.github.com>
2024-01-06 14:29:41 -0500

    Update EndGame.cs

--- UnityProjects/ReturnOfTheDemonLord/EndGame.cs
+++ UnityProjects/ReturnOfTheDemonLord/EndGame.cs
@@ -10,20 +10,16 @@ public class EndGame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Assign references
         player = FindObjectOfType<DemonLord>();
         fadePanel = FindObjectOfType<FadePanel>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player" && !collision.isTrigger)
         {
+            // When player boss rigidbody enters the finish game zone, transition to the final sequence and return to main menu
             player.gameOver = true;
             fadePanel.nextLevelName = "MainMenu";
             fadePanel.StartSceneTransition();
