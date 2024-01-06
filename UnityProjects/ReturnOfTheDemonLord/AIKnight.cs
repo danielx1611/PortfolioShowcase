345bc58b4d695f1dfe74f5a07180087979113240
RocketBlaster05 <82424308+RocketBlaster05@users.noreply.github.com>
2024-01-06 14:01:43 -0500

    Update AIKnight.cs

--- UnityProjects/ReturnOfTheDemonLord/AIKnight.cs
+++ UnityProjects/ReturnOfTheDemonLord/AIKnight.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class AIKnight : MonoBehaviour
 {
-    // Start is called before the first frame update
     [SerializeField] float m_speed = 4.0f;
     public int m_health;
     public int m_damage;
@@ -23,34 +22,38 @@ public class AIKnight : MonoBehaviour
     private Transform player;
     private bool m_noBlood = false;
 
-    // Use this for initialization
     void Start()
     {
+        // Set references
         m_animator = GetComponentInChildren<Animator>();
-        m_animator.speed = 0.5f;
         m_body2d = GetComponent<Rigidbody2D>();
         player = FindObjectOfType<DemonLord>().GetComponent<Transform>();
-        timeForAttackTimer = timeForAttack;
+
+        // Configure animator settings
+        m_animator.speed = 0.5f;
         m_animator.SetBool("noBlood", m_noBlood);
+
+        // Set timer to starting value
+        timeForAttackTimer = timeForAttack;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        // -- Handle movement --
-
+        // Disable collisions on death to prevent AI stopping player movement
         if (m_isDead)
         {
             gameObject.GetComponent<Collider2D>().enabled = false;
             return;
         }
 
+        // Count down until AI stops attacking to prevent trying to attack multiple times at once
         if (m_isAttacking)
         {
             timeForAttackTimer -= Time.deltaTime;
             if (timeForAttackTimer <= 0)
             {
+                // AI has finished attacking
                 timeForAttackTimer = timeForAttack;
                 m_isAttacking = false;
             }
@@ -62,11 +65,16 @@ public class AIKnight : MonoBehaviour
         {
             Vector3 directionToPlayer = (player.transform.position - transform.position).normalized;
 
+            // Calculate velocity of AI knight in direction of player
             m_body2d.velocity = new Vector2(directionToPlayer.x, directionToPlayer.y) * m_speed;
 
+            // If close enough, start attacking (does no damage, AI Knights are for visuals only)
             if (Vector2.Distance(player.transform.position, transform.position) < 2f && !m_isAttacking)
             {
+                // Stop AI from moving
                 m_body2d.velocity = Vector2.zero;
+
+                // Follow through with attack sequence
                 m_animator.SetTrigger("Attack1");
                 knightAttackSource = Instantiate(knightAttack, transform.position, transform.rotation).GetComponent<AudioSource>();
                 m_isAttacking = true;
@@ -74,31 +82,8 @@ public class AIKnight : MonoBehaviour
         }
         else
         {
+            // Player target out of range
             m_body2d.velocity = Vector2.zero;
-        }
-
-        // Swap direction of sprite depending on walk direction
-        /*if (m_body2d.velocity.x > 0)
-            transform.localScale = new Vector3(-sizeScale, sizeScale, sizeScale);
-        else if (m_body2d.velocity.x < 0)
-            transform.localScale = new Vector3(sizeScale, sizeScale, sizeScale);*/
-
-        // -- Handle Animations --
-
-/*        //Run
-        if (Mathf.Abs(m_body2d.velocity.x) > Mathf.Epsilon || Mathf.Abs(m_body2d.velocity.y) > Mathf.Epsilon)
-            m_animator.SetBool("isMoving", true);
-
-        //Idle
-        else
-            m_animator.SetBool("isMoving", false);*/
-    }
-
-    public void DamagePlayer()
-    {
-        if (Vector2.Distance(player.transform.position, transform.position) < 1f)
-        {
-            // playerDL.TakeDamage(m_damage);
         }
     }
 
@@ -109,23 +94,15 @@ public class AIKnight : MonoBehaviour
         {
             if (m_health <= 0)
             {
+                // Follow through with death sequence
                 m_animator.SetTrigger("Death");
                 m_isDead = true;
                 Instantiate(knightDeath, transform.position, transform.rotation);
-            }
-            else
-            {
-                // m_animator.SetTrigger("Hurt");
             }
         }
         if (knightAttackSource != null)
         {
             knightAttackSource.Stop();
         }
-    }
-
-    public void DamageAllEnemiesInRange()
-    {
-        // Redundant
     }
 }
