b9755167f065672ec9a3e8a276622b5f575a47b9
RocketBlaster05 <82424308+RocketBlaster05@users.noreply.github.com>
2024-01-05 22:11:03 -0500

    Update KillBorder.cs

--- UnityProjects/FightToSurvive/KillBorder.cs
+++ UnityProjects/FightToSurvive/KillBorder.cs
@@ -8,6 +8,7 @@ public class KillBorder : MonoBehaviour
 
     public PlayerStats playerStats;
 
+    // If the player enters the deadzone barrier, kill them.
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
